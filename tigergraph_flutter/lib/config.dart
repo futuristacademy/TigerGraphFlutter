1d52c24d8527b7845712d13bb4113dc30a4ac010
raghavauppuluri13 <sairagupp13@gmail.com>
2019-10-06 11:21:00 -0500

    removed config file

--- tigergraph_flutter/lib/config.dart
+++
@@ -1,7 +0,0 @@
-final String authKeyRaghava = 'Bearer 7rv807b6m7eeq0gmdj64bo17ov2qcehl';
-final String hostRaghava = 'testsocial.i.tgcloud.us:9000';
-
-String returnHttpUrl(String host, String vertexType, vertexID) //uses
-{
-  return 'https://$host/graph/vertices/$vertexType/$vertexID';
-}
